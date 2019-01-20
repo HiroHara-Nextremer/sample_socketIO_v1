a521bfd88f601af219d1e312bf91a1e1ef59a268
HiroHara-Nextremer <hiroaki.hara@nextremer.com>
2019-01-20 14:56:31 +0900

    add broadcasting message feature

--- index.js
+++ index.js
@@ -22,4 +22,8 @@ io.on('connection',(socket) => {
 		console.log(data);
 		io.sockets.emit('chat',data);
 	});
+
+	socket.on('typing',(data) => {
+		socket.broadcast.emit('typing',data);
+	});	
 });
--- public/chat.js
+++ public/chat.js
@@ -2,11 +2,12 @@
 const socket = io.connect("http://localhost:4000");
 
 // Query DOM
-let message, handle, btn, output;
+let message, handle, btn, output, typing;
 message = document.getElementById("message");
 handle = document.getElementById("handle");
 btn = document.getElementById("btn");
 output = document.getElementById("output");
+typing = document.getElementById('nowtyping');
 
 // Emit events 
 btn.addEventListener('click',() => {
@@ -16,8 +17,17 @@ btn.addEventListener('click',() => {
 	});
 });
 
+message.addEventListener('keypress',() => {
+	socket.emit('typing',handle.value);
+});
+
 // Listen for events coming back from server 
 socket.on('chat',(data) => {
 	console.log(data);
+	typing.innerHTML = "";
 	output.innerHTML += '<p><strong>' + data.handle + ' :</strong> ' + data.message + '</p>';
+});
+
+socket.on('typing',(data) => {
+	typing.innerHTML = '<p>' + data + ' is tying a message...</p>';
 });
